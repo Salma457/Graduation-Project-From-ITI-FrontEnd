cfe7064c7236abd201e0f3e4c711dbad74279ffe
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-06-17 15:05:03 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-amber-400 p-3'>
+        <h2>Welcome Abdo!</h2>
+        <p className='text-blue-500'> hello</p>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
