2dfbb014691f04e1759f09b94e6dded13f29816f
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-06-22 16:11:29 +0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import store from './store.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <AuthProvider>
+      {/* <AuthProvider> */}
         <App />
-      </AuthProvider>
+      {/* </AuthProvider> */}
     </Provider>
   </StrictMode>
 );
