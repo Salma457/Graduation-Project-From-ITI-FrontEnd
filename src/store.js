ffe9ff2f1d177e9dae8a1dc0f9151de05ac0077c
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-06-22 17:58:53 +0300

    adding the itian slice to the store

--- src/store.js
+++ src/store.js
@@ -8,6 +8,7 @@ import chatReducer from './pages/Employer/chatSlice';
 import { chatApi } from './api/chatApi';
 import itianProfileReducer from './store/itianProfileSlice';
 import employerProfileReducer from './store/employerProfileSlice';
+import itianReducer from './Posts/store/itianSlice.js';
 
 const store = configureStore({
   reducer: {
@@ -19,6 +20,7 @@ const store = configureStore({
     chat: chatReducer,
     itianProfile: itianProfileReducer,
     employerProfile: employerProfileReducer,
+    itian: itianReducer,
     [chatApi.reducerPath]: chatApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
