2042edfe0416ccf2176fcf0a2a139075eeba9cfd
arwaemam2 <arwaaemamm@gmail.com>
2025-06-26 12:04:33 +0300

    profile-repot

--- src/components/EmployerNavbar.jsx
+++ src/components/EmployerNavbar.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { FaBell, FaUser, FaBriefcase, FaSignOutAlt, FaBars, FaTimes, FaFileAlt, FaChartBar } from "react-icons/fa";
+import { FaBell, FaUser, FaBriefcase, FaSignOutAlt, FaBars, FaTimes, FaFileAlt, FaExclamationTriangle } from "react-icons/fa";
 import "../css/Navbar.css";
 
 function EmployerNavbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [jobsDropdownOpen, setJobsDropdownOpen] = useState(false);
   const [reportsDropdownOpen, setReportsDropdownOpen] = useState(false);
   const [notificationOpen, setNotificationOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
-  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+  const toggleJobsDropdown = () => setJobsDropdownOpen(!jobsDropdownOpen);
   const toggleReportsDropdown = () => setReportsDropdownOpen(!reportsDropdownOpen);
   const toggleNotifications = () => setNotificationOpen(!notificationOpen);
 
@@ -23,14 +23,14 @@ function EmployerNavbar() {
     <>
       <nav className="navbar">
         <div className="logo-container">
-          <img src="public/logo.png" alt="Logo" className="logo-img " />
+          <img src="public/logo.png" alt="Logo" className="logo-img" />
           <div className="logo-text">
             <Link to="/">ITIAN</Link>
           </div>
         </div>
 
         <div className={`menu ${isOpen ? "open" : ""}`}>
-          <div className={`dropdown ${dropdownOpen ? "open" : ""}`} onClick={toggleDropdown}>
+          <div className={`dropdown ${jobsDropdownOpen ? "open" : ""}`} onClick={toggleJobsDropdown}>
             <button className="dropbtn">
               <FaBriefcase className="nav-icon" /> Jobs
             </button>
@@ -40,13 +40,14 @@ function EmployerNavbar() {
             </div>
           </div>
 
+          {/* Added Reports Dropdown */}
           <div className={`dropdown ${reportsDropdownOpen ? "open" : ""}`} onClick={toggleReportsDropdown}>
             <button className="dropbtn">
-              <FaChartBar className="nav-icon" /> Reports
+              <FaExclamationTriangle className="nav-icon" /> Reports
             </button>
             <div className="dropdown-content">
               <Link to="/reports/create"><FaFileAlt className="dropdown-icon" /> Create Report</Link>
-              <Link to="/reports/my-reports"><FaFileAlt className="dropdown-icon" /> My Reports</Link>
+              <Link to="/my-reports"><FaFileAlt className="dropdown-icon" /> My Reports</Link>
             </div>
           </div>
 
